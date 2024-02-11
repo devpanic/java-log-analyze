7be02baf336bcb9f036c49d160c70e806afcf2bc
devpanic <yeon.dev.8@gmail.com>
2024-02-11 16:16:46 +0900

    feat, doc : read file with each line / add README.md

--- src/main/java/fileAnalyze/LogFileParsing.java
+++ src/main/java/fileAnalyze/LogFileParsing.java
@@ -15,10 +15,10 @@ public class LogFileParsing {
     public LogFileParsing() throws IOException {
         logPath = new File("./log");
         logs = logPath.listFiles();
-        
+
         for (File log : logs) {
             openStream(log);
-            // parse data
+            readStream();
             closeStream();
         }
     }
@@ -31,5 +31,31 @@ public class LogFileParsing {
     public void closeStream() throws IOException {
         bufReader.close();
         fReader.close();
+    }
+
+    /*
+     * before : [200][http://sist.co.kr/find/books?key=mongodb&query=sist][ie][2024-02-07 09:35:16]
+     * After : 200 books=mongodb ie 2024-02-07 09
+     */
+    public void readStream() throws IOException {
+        String temp = null;
+        String[] parsedLine = null;
+
+        while ((temp = bufReader.readLine()) != null) {
+            parsedLine = refineString(temp);
+        }
+    }
+
+    public String[] refineString(String temp) {
+        System.out.println("before : " + temp);
+        temp = temp.replaceAll("\\[", "")
+                .replaceAll("\\]", " ")
+                .replaceAll("http://sist.co.kr/find/", "")
+                .replaceAll("\\?key", "")
+                .replaceAll("&query=sist", "")
+                .replaceAll("(\\d{2}):(\\d{2}):(\\d{2})", "$1");
+        System.out.println("After : " + temp);
+
+        return temp.split(" ");
     }
 }
